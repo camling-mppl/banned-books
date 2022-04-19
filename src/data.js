02de98a60c1ec1d124f94350fb3f7974cd036986
MPPL <94387889+MPPL-DEV@users.noreply.github.com>
2022-04-19 16:22:14 -0500

    Created data.js with beginning of database

---
+++ src/data.js
@@ -0,0 +1,15 @@
+
+const data = [
+{"id": 1, "title":"Gender Queer", "author":"Maia Kobabe", "ibsn":"","image":"", "description":"", "reasons":"Banned, challenged, and restricted for LGBTQIA+ content, and because it was considered to have sexually explicit images"},
+{"id": 2, "title":"Lawn Boy", "author":"Jonathan Evison", "ibsn":"","image":"", "description":"", "reasons":"Banned and challenged for LGBTQIA+ content and because it was considered to be sexually explicit"},
+{"id": 3, "title":"All Boys Aren't Blue", "author":"George M. Johnson", "ibsn":"","image":"", "description":"", "reasons":"Banned and challenged for LGBTQIA+ content, profanity, and because it was considered to be sexually explicit"},
+{"id": 4, "title":"Out of Darkness", "author":"Ashley Hope Perez", "ibsn":"","image":"", "description":"", "reasons":"Banned, challenged, and restricted for depictions of abuse and because it was considered to be sexually explicit"},
+{"id": 5, "title":"The Hate U Give", "author":"Angie Thomas", "ibsn":"","image":"", "description":"", "reasons":"Banned and challenged for profanity, violence, and because it was thought to promote an anti-police message and indoctrination of a social agenda"},
+{"id": 6, "title":"The Absolutely True Diary of a Part-Time Indian", "author":"Sherman Alexie", "ibsn":"","image":"", "description":"", "reasons":"Banned and challenged for profanity, sexual references and use of a derogatory term"},
+{"id": 7, "title":"Me and Earl and the Dying Girl ", "author":"Jesse Andrews", "ibsn":"","image":"", "description":"", "reasons":"Banned and challenged because it was considered sexually explicit and degrading to women"},
+{"id": 8, "title":"The Bluest Eye", "author":"Toni Morrison", "ibsn":"","image":"", "description":"", "reasons":"Banned and challenged because it depicts child sexual abuse and was considered sexually explicit"},
+{"id": 9, "title":"This Book is Gay", "author":"Juno Dawson", "ibsn":"","image":"", "description":"", "reasons":"Banned, challenged, relocated, and restricted for providing sexual education and LGBTQIA+ content."},
+{"id": 10, "title":"Beyond Magenta ", "author":"Susan Kuklin", "ibsn":"","image":"", "description":"", "reasons":"Banned and challenged for LGBTQIA+ content and because it was considered to be sexually explicit. "},
+];
+
+export default data;
