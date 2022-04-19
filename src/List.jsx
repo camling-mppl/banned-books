9131696bb7d777598d5c1ec165c6b310b6bb051e
MPPL <94387889+MPPL-DEV@users.noreply.github.com>
2022-04-19 17:00:35 -0500

    Start creating the list template

--- src/List.jsx
+++ src/List.jsx
@@ -3,9 +3,20 @@ import data from './data';
 function List()
 {
 
+
+
     return (
         <>
         <h1>The top 10 Banned Books of 2021</h1>
+        <div className='listContainer'>
+        {data.map((book,index) => ( 
+        <div className='listItem' key={book.id} onClick={()=> {}}>
+            <h2>{book.title} </h2>
+            <img style={{maxWidth: '50px'}} alt={book.title} src={book.image} />
+            
+        </div>
+        ))}
+      </div>
         </>
     );
 }
