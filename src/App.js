77141fd12bde895ff7a34cac71bb62a0a6093db1
MPPL <94387889+MPPL-DEV@users.noreply.github.com>
2022-04-19 16:21:26 -0500

    Added List import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-
+import List from './List';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+      <h1>Banned Books</h1>
       </header>
+      <List/>
     </div>
   );
 }
