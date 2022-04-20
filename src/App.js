29ff69aa3ba433b362961d7428e018fdcc6a8c91
MPPL <94387889+MPPL-DEV@users.noreply.github.com>
2022-04-20 14:11:39 -0500

    Created useState consts and sent them as props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import List from './List';
+import Detail from './Detail';
+
 function App() {
+
+const [displayList, setDisplay] = useState(true);
+const [displayDetails, setDetails] = useState(false);
+const [currentId, setId] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
       <h1>Banned Books</h1>
       </header>
-      <List/>
+      {displayList === true && <List setId={setId} setDisplay={setDisplay} setDetails={setDetails}/>}
+      {displayDetails === true && currentId !== null && <Detail setId={setId} setDisplay={setDisplay} setDetails={setDetails} id={currentId}/>}
     </div>
   );
 }
+
 
 export default App;
